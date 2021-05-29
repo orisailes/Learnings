4b1a9dc754fc7767c75a55a4f2c2d195836432c3
orisailes <ori1sailes@gmail.com>
2021-05-29 20:43:04 +0300

    file adding

---
+++ typescript/jest-ts/src/__tests__/isogram.test.ts
@@ -0,0 +1,26 @@
+import isogram from '../functions/isogram'
+
+test('isogram test', () => {
+    const funcResult = isogram("Dermatoglyphics")
+    const result = true
+    expect(funcResult)
+        .toBe(result)
+})
+test('isogram test', () => {
+    const funcResult = isogram(4231)
+    const result = false
+    expect(funcResult)
+        .toBe(result)
+})
+test('isogram test', () => {
+    const funcResult = isogram(true)
+    const result = false
+    expect(funcResult)
+        .toBe(result)
+})
+test('isogram test', () => {
+    const funcResult = isogram('moOse')
+    const result = false // -- ignore letter case
+    expect(funcResult) 
+        .toBe(result)
+})
---
+++ typescript/jest-ts/src/functions/isogram.ts
@@ -0,0 +1,22 @@
+// Ex6.4 - isogram
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement
+// a function that determines whether a string that contains only letters is an isogram. Assume the
+// empty string is an isogram. Ignore letter case.
+// isIsogram("Dermatoglyphics") == true
+// isIsogram("aba") == false
+// isIsogram("moOse") == false // -- ignore letter case
+
+export default function isogram(string) {
+    if(typeof string !== 'string') return false
+    string = string.toLowerCase().split('');
+    let helper = string;
+    for (let i = 0; i < string.length; i++) {
+        for (let x = 0; x < string.length; x++) {
+            helper = string.slice(0, i) + string.slice(i + 1)
+            if (string[i].includes(helper[x])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
