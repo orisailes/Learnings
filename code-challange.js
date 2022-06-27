1346d62fe9883b06f97336e2371a32c2d7a849ec
Ori Sailes <77921279+orisailes@users.noreply.github.com>
2022-06-27 15:31:11 +0300

    Create code-challange.js

---
+++ code-challange.js
@@ -0,0 +1,31 @@
+// create a function that accepts two arrays, arrA and arrB and an index.
+// The function should merge arrB into arrA at the given index.
+// example:
+// const arrA = [1, 2, 3]
+// const arrB = [7, 7, 7]
+// mergeAtIndex(arrA, arrB, 1) // [1,7,7,7, 2, 3]
+
+function mergeAtIndex(arrA, arrB, idx) {
+  return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write a function that, given a string of text (possibly with punctuation // and line-breaks), returns an array of the top-3 most occurring words, in // descending order of the number of occurrences.
+
+function top3words(text) {
+  return;
+}
